f7a9deb05fa66c6f761bf228895ce44c2fd5e29f
sean9392 <seancls@hotmail.co.uk>
2018-02-12 14:28:15 +0000

    Testing, testing

---
+++ NeonCrisis/Assets/Scripts/enemy_destroy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_destroy : MonoBehaviour {
+
+	GameObject Explosion;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	void OnCollisionEnter2D (Collision2D col) {
+
+		if (col.gameObject.tag == "Pew") {
+
+			GameObject Explosion = Instantiate (Resources.Load ("Explosion_1"), new Vector3(0.03f, -1.42f, -1), Quaternion.identity)  as GameObject;
+
+			Destroy(gameObject);
+		}
+
+
+	}
+}
