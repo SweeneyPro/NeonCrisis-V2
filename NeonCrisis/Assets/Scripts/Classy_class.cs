daff56a55a53606eb86f43e2ee4c3df41470adf6
unknown <sweeney.pro@hotmail.co.uk>
2018-02-12 16:14:22 +0000

    Began enemy

---
+++ NeonCrisis/Assets/Scripts/Classy_class.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classy_class : MonoBehaviour {
+
+	public GameObject pew;
+	public float cooldown;
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		cooldown += Time.deltaTime;
+
+		if (cooldown > 1) {
+			Instantiate (pew, transform.position, Quaternion.identity);
+			cooldown = 0;
+
+		}
+
+	}
+}
---
+++ NeonCrisis/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+
+	public float move_speed;
+	Rigidbody2D rigidbody;
+	public Transform shoot_position;
+	public GameObject bullet_object;
+	public float next_shot_delay;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Handle_User_Input();
+	}
+
+	void Handle_User_Input()
+	{
+		float hor = Input.GetAxis("Horizontal");
+		float ver = Input.GetAxis("Vertical");
+		Vector3 move = new Vector3(hor * move_speed, ver * move_speed, 0);
+		rigidbody.velocity = move;
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Fire();
+		}
+	}
+
+	void Fire()
+	{
+		Instantiate(bullet_object, shoot_position.position, Quaternion.identity);
+	}
+}
