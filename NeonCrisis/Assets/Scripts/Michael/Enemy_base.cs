322217462977a364f773066d0897a8075af18748
unknown <sweeney.pro@hotmail.co.uk>
2018-02-19 18:09:58 +0000

    I dont even know

--- NeonCrisis/Assets/Scripts/Michael/Enemy_base.cs
+++ NeonCrisis/Assets/Scripts/Michael/Enemy_base.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEditor.Animations;
 public class Enemy_base : MonoBehaviour {
 
+    [System.Serializable]
     public struct Enemy_Information_Instance
     {
         public float move_speed, fire_rate, fire_speed, start_time;
         public int health;
         public AnimatorController animation_controller;
         public string fire_pattern_type;
-        
+        public GameObject bullet_type;
+        public Base_Fire_Pattern fire_pattern;
+
     }
 
     public string enemy_name;
@@ -24,8 +28,10 @@ public class Enemy_base : MonoBehaviour {
     public string fire_pattern_type;
     List<float> times = new List<float>();
     int curent_time_index = 0; // dont need to record index if we removing elements anyway right?
-
+    Base_Fire_Pattern fire_pattern;
     float timer = 0;
+
+    
     public List<Enemy_Information_Instance> BehaviourSets = new List<Enemy_Information_Instance>();
 
     // Use this for initialization
@@ -46,8 +52,8 @@ public class Enemy_base : MonoBehaviour {
     void Assign_Local_Variables()
     {
         timer += Time.deltaTime;
-
-        if(timer > times[0])
+        
+        if(timer > 5)
         {
             timer = 0;
             
@@ -57,9 +63,10 @@ public class Enemy_base : MonoBehaviour {
             start_time = BehaviourSets[0].start_time;
             health = BehaviourSets[0].health;
             animation_controller = BehaviourSets[0].animation_controller;
+            GetComponent<Animator>().runtimeAnimatorController = animation_controller;
             fire_pattern_type = BehaviourSets[0].fire_pattern_type;
 
-            times.RemoveAt(0);
+            //times.RemoveAt(0);
             BehaviourSets.RemoveAt(0);
         }
         //assign from current_time_index
@@ -74,7 +81,7 @@ public class Enemy_base : MonoBehaviour {
         collider_size = collidersize;
     }
 
-    public virtual void EnemyBehaviourConstructor(float movespeed, float firerate, float firespeed, float starttime, int health, AnimatorController animationcontroller, string _fire_pattern_type)
+    public virtual void EnemyBehaviourConstructor(float movespeed, float firerate, float firespeed, float starttime, int health, AnimatorController animationcontroller, string _fire_pattern_type, GameObject bullettype, Base_Fire_Pattern firepattern)
     {
         Enemy_Information_Instance behaviour_set_instance;
         behaviour_set_instance.move_speed = movespeed;
@@ -84,7 +91,8 @@ public class Enemy_base : MonoBehaviour {
         behaviour_set_instance.health = health;
         behaviour_set_instance.animation_controller = animationcontroller;
         behaviour_set_instance.fire_pattern_type = _fire_pattern_type;
-
+        behaviour_set_instance.bullet_type = bullettype;
+        behaviour_set_instance.fire_pattern = firepattern;
         BehaviourSets.Add(behaviour_set_instance);
     }
 }
