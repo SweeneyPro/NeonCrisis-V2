492b0e6d25124fafceb7809999d9026578faae1d
Joseph Adams <WASDMagician@gmail.com>
2018-02-20 11:21:21 +0000

    Revert "Base_Fire_Pattern_Update"
    This reverts commit 125534b95449a219e0c9f3e8ee3b40983393ac1d.

--- NeonCrisis/Assets/Scripts/Joe/Base_Fire_Pattern.cs
+++ NeonCrisis/Assets/Scripts/Joe/Base_Fire_Pattern.cs
@@ -2,62 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Base_Fire_Pattern : MonoBehaviour
-{
+public class Base_Fire_Pattern : MonoBehaviour {
 
-    public enum fire_patterns { forward, circle, loop, homing };
-    public fire_patterns fire_pattern;
-    public GameObject bullet;
-    float next_fire_time, fire_delay;
-
-    // Use this for initialization
-    void Start()
-    {
-        fire_delay = 1;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Time.fixedTime > next_fire_time)
-        {
-            Fire();
-            next_fire_time = Time.fixedTime + fire_delay;
-        }
-    }
-
-    public void Fire()
-    {
-        if (fire_pattern == fire_patterns.forward)
-        {
-            Instantiate(bullet, this.transform.position, this.transform.rotation);
-        }
-        else if (fire_pattern == fire_patterns.circle)
-        {
-            Fire_Circle();
-        }
-        else if (fire_pattern == fire_patterns.homing)
-        {
-
-        }
-        else if (fire_pattern == fire_patterns.loop)
-        {
-
-        }
-    }
-
-    void Fire_Circle()
-    {
-
-    }
-
-    IEnumerator Fire_Loop()
-    {
-        yield return new WaitForSeconds(0);
-    }
-
-    IEnumerator Fire_Homing()
-    {
-        yield return new WaitForSeconds(0);
-    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
