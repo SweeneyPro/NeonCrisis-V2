dfc0ffda157d7bba4ad127f4bde0df1810cbfd90
Joseph Adams <WASDMagician@gmail.com>
2018-02-20 10:56:09 +0000

    Revert "Merge branch 'master' of https://github.com/SweeneyPro/NeonCrisis-V2"
    This reverts commit 22b9f8e6b877a317675e98240edc23f0071fc58f, reversing
    changes made to 125534b95449a219e0c9f3e8ee3b40983393ac1d.

---
+++ NeonCrisis/Assets/Editor/Distance_Between.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Distance_Between : MonoBehaviour {
+
+    [MenuItem("Tools/Measure Distance")]
+	public static void Measure_Distance()
+    {
+        GameObject[] objects = Selection.gameObjects;
+        List<Vector3> positions = new List<Vector3>();
+        List<float> widths = new List<float>();
+        float sum = 0;
+        for(int i = 0; i < objects.Length; i++)
+        {
+            positions.Add(objects[i].transform.position);
+            SpriteRenderer renderer = objects[i].GetComponent<SpriteRenderer>();
+            widths.Add(renderer.size.x);
+            sum += widths[i];
+        }
+        sum /= 2;
+        float dist = Vector3.Distance(positions[0], positions[1]);
+        print(dist - sum);
+
+    }
+}
