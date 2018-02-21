80cf539821a4c758ed2286556b132599473190a0
Joseph Adams <WASDMagician@gmail.com>
2018-02-21 13:13:39 +0000

    MICHAELS PROPER TOOL

--- NeonCrisis/Assets/Editor/Folder_Creator.cs
+++ NeonCrisis/Assets/Editor/Folder_Creator.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class Folder_Creator : MonoBehaviour {
-    
+public class Folder_Creator : EditorWindow {
+
+    string folder_name;
+
     [MenuItem("Tools/Michael/Make Folder")]
-    public static void Create_Folder()
+    public static void Show_Window()
     {
-        AssetDatabase.CreateFolder("Assets", "Michael_Is_Fucking_Useless");
+        EditorWindow.GetWindow(typeof(Folder_Creator));
+    }
+
+    private void OnGUI()
+    {
+        folder_name = EditorGUILayout.TextField("Folder Name", folder_name);
+        if(GUILayout.Button("Create Folder"))
+        {
+            AssetDatabase.CreateFolder("Assets", folder_name);
+        }
     }
 }
