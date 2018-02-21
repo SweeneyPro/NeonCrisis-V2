3ebb47966f3da7a4c9fbb66dee4062980640f45a
sean9392 <seancls@hotmail.co.uk>
2018-02-21 12:15:00 +0000

    Laser shit for you Michael

---
+++ NeonCrisis/Assets/Scripts/Michael/LaserAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserAnimation : MonoBehaviour {
+
+    [SerializeField]
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown("p"))
+        {
+            anim.SetBool("Active", true);
+            anim.speed = 1;
+            Debug.Log("Active");
+        }
+        else if(Input.GetKeyUp("p"))
+        {
+            anim.SetBool("Active", false);
+            anim.speed = 1;
+            Debug.Log("Deactive");
+        }
+		
+	}
+}
