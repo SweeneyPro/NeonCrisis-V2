6aabcabef9998a250238d1b2bbb5d436283852d9
JamieCollinge <jamie.collinge91@gmail.com>
2018-02-23 18:54:26 +0000

    End of day
    Jamie

--- NeonCrisis/Assets/Scripts/Joe/Base_Fire_Pattern.cs
+++ NeonCrisis/Assets/Scripts/Joe/Base_Fire_Pattern.cs
@@ -16,6 +16,7 @@ public class Base_Fire_Pattern : MonoBehaviour {
     /*
      * NEEDS TO BE UPDATED TO USE MULTIPLE STATES LIKE CURVE_MOVEMENT
      */
+<<<<<<< HEAD
 
     public void Setup(float _fire_speed, float _bullet_speed, string _fire_type)
     {
@@ -24,6 +25,8 @@ public class Base_Fire_Pattern : MonoBehaviour {
         fire_pattern_type = _fire_type;
     }
 
+=======
+>>>>>>> f4560cef837de0d97428f6aa88ad7b1d92fb49d8
     private void Update()
     {
         if(Time.fixedTime > next_fire_time)
